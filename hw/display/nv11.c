f304406d5c5db5e5d383d5fb28cdc22ad645c36c
tisenu100 <103323979+tisenu100@users.noreply.github.com>
2026-09-17 20:22:53 +0300

    hw/display/nv11: Fix small typo

--- hw/display/nv11.c
+++ hw/display/nv11.c
@@ -23,7 +23,7 @@
  */
 
 /*
- * The GF2 MX400 variant used is the Abit Siluto. Normally it's AGP but we hack it as PCI.
+ * The GF2 MX400 variant used is the Abit Siluro. Normally it's AGP but we hack it as PCI.
  * The VBIOS is named MX4_0153.rom
  * 
  * Specs:
@@ -453,7 +453,7 @@ static void nv11_realize(PCIDevice *dev, Error **errp)
 
     memset(s->bar0_flat, 0, sizeof(s->bar0_flat));
 
-    s->vga.vram_size_mb = NV11_BAR1_VRAM_SIZE_MB; /* Abit Siluto has 64MB of VRAM */
+    s->vga.vram_size_mb = NV11_BAR1_VRAM_SIZE_MB; /* Abit Siluro has 64MB of VRAM */
     if (!vga_common_init(&s->vga, OBJECT(dev), errp)) {
         return;
     }
